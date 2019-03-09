810cfc26b99450c0f25b5ff5ed83c17d2d70e06c
Leo Taylor <leotaylor723@gmail.com>
2019-03-09 12:58:40 -0600

    css files

---
+++ src/components/Bring/Bring.js
@@ -0,0 +1,126 @@
+import React from 'react';
+
+import './Bring.css';
+
+class Bring extends React.Component {
+
+  render () {
+
+    return (
+      <div>
+        <div className="bandPhoto text-center"></div>
+        <div className="bringDiv text-left">
+          <h2 className="title"><strong>AyerWaves Rules</strong></h2>
+          <p>
+            ** No firearms or weapons <br/>
+            ** No fireworks, explosives, candles, or sky lanterns<br/>
+            ** No fire toys – All fire performers must be pre-approved <br/>
+            ** No open flames (White Gas/Propane is ok) <br/>
+            ** No glass containers of any kind. (Make sure it is in cans or boxes!) We do recycle.<br/>
+            ** No outside alcohol in the staging area (We will have beer available for sale) <br/>
+            ** No professional photography equipment or cameras with detachable lenses. <br/>
+            ** No unauthorized vending will not be tolerated (stationary or roaming) <br/>
+            ** No outside solicitations including handbills, samples, and,  giveaways. <br/>
+            ** No illegal drugs or drug paraphernalia <br/>
+            ** No amplified sound and guerrilla sound systems. They will be shut down and confiscated. <br/>
+            ** No campfires <br/>
+            ** No video recording of any performances. Just enjoy the moment!<br/>
+            ** No motorized vehicles including moped, golf cart, vespa, and other similar vehicles <br/>
+            ** No laser pens or similar focused-light devices. <br/>
+            ** No unauthorized motorized and/or remote control aerial devices, toys, cameras or equipment (drones, toy planes, etc.). <br/>
+            ** No confetti, please (every piece needs to get picked up!).<br/>
+          </p>
+          <p>
+            <strong>You can bring your own alcohol to the event! Please leave it at your campsite. (No Glass please)</strong><br/>
+            No dogs inside the performance areas. <br></br><br></br>
+            No wristband, no admittance (Violation is trespassing and subject to eviction and arrest.) Print out your tickets! We exchange ticket for wristband at the gate!<br></br><br></br>
+            We will have awesome food vendors!! Please remember that some of the vendors only except cash, so be prepared for different forms of payment. (We have no control over the vendor's payment methods.)<br></br><br></br>
+          </p>
+          <h3>Waste Management</h3>
+          <p>
+          Bring extra trash and recycling bags to take your personal and camp waste off-site with you, and do your part by cleaning up any random garbage that you see, as well as from your campsite before leaving. We appreciate your help in keeping the farm clean!
+          </p>
+          <p>
+          (Purchasers of AyerWaves Entertainment tickets voluntarily assume all risks and danger incidental to any event for which tickets are issued and waives all claims against the event, the event sponsor, the event brand, the owner of the facility, the ticket producer or issuer, any staff,  volunteer, and performer. Admission is subject to compliance with all laws and facility rules and the ticket holder consents to reasonable examinations and searches to ensure compliance. Failure to comply with the law or facility rules may result in refusal of admission or expulsion. No unauthorized recording of event where prohibited. No refunds or exchanges.)
+          </p> <br/>
+          <h2 className="title"><strong>AyerWaves 2019 Packing List </strong></h2>
+          <h3 className="pack">
+            AyerWaves tickets — print ‘em out now!<br/>
+            Directions — print ‘em out now! We are at a cool location hidden on the side of a back road! <br/>
+            Tent <br/>
+            Tarp <br/>
+            Blanket<br/>
+            Chair<br/>
+            Ground pad <br/>
+            Sunglasses <br/>
+            Headlamp <br/>
+            Camera <br/>
+            Phone charger <br/>
+            Comfy socks <br/>
+            Rain gear<br/>
+            Cool weather clothes<br/>
+            Shoes<br/>
+            Dancing Shoes<br/>
+            Sunscreen <br/>
+            Poison ivy cream <br/>
+            Chapstick <br/>
+            Water bottle<br/>
+            Trash and Recycle Bags<br/>
+          </h3>
+          <h3>
+            Acoustic guitar, stand up bass, fiddle, mandolin, bongos (We love to jam!) <br/>
+          </h3>
+          <h3>
+            CASH!!! (Most vendors only except cash, and we have no control over any of their payment methods.)<br/>
+          </h3>
+          <p>
+            Campsites are first come first serve. Some campsites are considered better then others, but fun will be had throughout, so do not worry! <br></br>
+            We support car camping! <br/>
+            We have some cool spots for hammock camping!<br></br>
+            If you go hiking around the grounds please watch out for snakes, bears, hogs, spiders, sticks that poke, and other ouch stuff in the woods. <br/>
+            Remember most things in the woods are cool! If you do not bother them they will not bother you! <br/>
+          </p>
+          <h3><strong>Camping and Parking</strong></h3>
+          <h4>Camping</h4>
+          <p>
+          All tickets have walk-in camping included. Camping is first-come, first-served. Please be mindful of your overall footprint and your neighbors.
+          </p><br></br>
+          <p>AyerWaves Festival participants may set up camp anywhere in the campground that is not marked out by event staff for parking, environments, or art. Please keep in mind that vehicles are not permitted on-site for gear drops or pick-ups.</p><br></br>
+          <p>Guests are encouraged to enjoy the musical offerings of AyerWaves and are not allowed to set up renegade sound stages. If you have a generator in your camp set up, we graciously ask you to enforce a short quiet time in the late evenings from 12 AM (midnight) – 8 AM to allow for friends and families to rest. </p><br></br>
+          <h3>Camping and Parking</h3>
+          <p>All tickets include walk-in camping.  However, we have an option for people who enjoy camping directly next to your car. Each area is approximately 10' x 25', and comfortably allows enough room for your vehicle and one tent.  Car camping is located outside of the festival gates, with very few inside car camping spots. There is a very short walk to the entrance. Spaces assigned on a first-come, first-served basis. Everyone in your vehicle must have purchased a festival pass in order to enter the festival with a car camping pass. There are no “in and out” privileges for car camping pass vehicles. (You will be able to go back and forth from the festival to your car camping spot.  However, you will NOT be able to remove your car and come back, once you have initially entered the car camping area.)  If the total length of your vehicle plus anything attached to it is 18' or longer, it is considered an RV and you must purchase an RV Pass. </p><br></br>
+          <h3>Parking</h3>
+          <p>EVERY VEHICLE arriving on-site must have a vehicle parking pass or car camping pass.  Camping will not be allowed in the General Parking Lot or in the parking lot by the main entrance.  We have a very limited amount of parking on-site and we highly encourage you to carpool. Any vehicle longer than 18 feet arriving onto the festival site will need to purchase an RV Parking Pass. </p><br></br>
+          <h3>RV Parking</h3>
+          <p>RVs are permitted to camp on-site in designated area with a purchased RV Parking Pass. RVs are considered vehicles over 18 feet in length. All RV pass holders get super awesome parking spots! (We do not have RV hook ups) </p><br></br>
+          <p><strong>Parking is limited except to full event ticket goers, so carpool if possible. </strong> <br/><br/>
+            For Single Day pass holders that do not get parking spots close, DO NOT WORRY! We expect great weather, so the short walk will be nice!<br/>
+          </p><br/><br/>
+          <h3>Lost and Found</h3>
+          <p>Lost and Found is located at HQ (the info/security central/volunteer booth). Valuable items – including wallets, ID, cameras and phones – will be held after the event, and we will be proactive in returning them to their owners. This being said, please keep valuables at home, do your personal inventory before leaving the site, and be prompt in searching for anything lost as soon as you realize it is gone! And – if you find something without its owner – please bring it over to Lost and Found.</p>
+          <h3>Banned Items & Activities</h3>
+          <p>Festival attendees must abide by all local, state, and federal laws. NO ALCOHOL OR DRUG SALES (including nitrous oxide) tolerated anywhere on the festival site or farm property. Anyone caught selling or distributing illicit substances will immediately have their wristband cut and be ejected from the festival. Further legal repercussions are likely. If it’s illegal outside the festival, it’s illegal inside, too. </p><br/>
+          <p><strong>Zero Tolerance for Drug Use or Possession </strong><br/>
+              The use or possession of any illegal drugs will not be tolerated anywhere inside or outside the venue. Do not bring any illegal substances to the event; violators will be prosecuted to the fullest extent of the law. If you are found intoxicated at entry, security will not allow you to enter the venue. Event staff reserves the right to refuse entry to anyone. Be responsible, and make smart choices. </p><br/><br/>
+          <p>We are committed to fostering a relaxed atmosphere, but there are certain guidelines we must abide by in order to ensure a safe and fun festival for everyone.  Help us help you by keeping fire lanes clear, notifying staff of any hazards, and communicating in times of distress – we’re on the same team! Look out for one another, stay hydrated, take breaks, wear sunscreen, and most of all, have fun!</p>
+        </div>
+        <footer>
+          <p>AyerWaves Entertainment © 2018 All Rights Reserved<br/>
+          Contact: ayerwavesmusic@gmail.com<br/>
+          </p>
+          <p>
+          A Sound Farm Events Production<br></br>
+            <a href="https://soundfarm.us/">www.soundfarm.us</a>
+          </p>
+          <div>
+            <a href="https://www.facebook.com/AyerWavesMusic/"><i className="fab fa-facebook-f icon"></i></a>
+            <a href="https://www.youtube.com/channel/UCbzDiCYcUFJaULli4zTESwQ"><i className="fab fa-youtube icon"></i></a>
+            <a href="https://www.instagram.com/ayerwavesmusic"><i className="fab fa-instagram icon"></i></a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Bring;
